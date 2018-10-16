b7bb3868dd3d6060225b43a3e54b455d70c28d3f
Holly Deng <holly199975@gmail.com>
2018-10-17 01:17:43 +0800

    抽取string,語言轉換

--- app/src/main/java/com/example/holly/bmi/Bmi.java
+++ app/src/main/java/com/example/holly/bmi/Bmi.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,13 +14,29 @@ public class Bmi extends AppCompatActivity {
 
     private EditText edWeight;
     private EditText edHeight;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+        findViews();
+    }
+
+    private void findViews() {
         edWeight = findViewById(R.id.ed_weight);
         edHeight = findViewById(R.id.ed_height);
+        help = findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Bmi","help");
+                new AlertDialog.Builder(Bmi.this)
+                        .setMessage(R.string.bmi_info)
+                        .setPositiveButton(R.string.ok,null)
+                        .show();
+            }
+        });
     }
 
     public void bmi(View view){
@@ -32,11 +49,11 @@ public class Bmi extends AppCompatActivity {
        float height = Float.parseFloat(h);
        float bmi = weight/(height*height);
        Log.d("Bmi",bmi+" ");
-        Toast.makeText(this,"Your Bmi is"+ bmi,Toast.LENGTH_LONG).show();
-        new AlertDialog.Builder(this).setMessage("Your BMI is" + bmi)
-                .setTitle("BMI")
-                .setPositiveButton("OK",null)
-                .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+        Toast.makeText(this,getString(R.string.your_bmi_is)+ bmi,Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this).setMessage((int) (R.string.your_bmi_is + bmi))
+                .setTitle(R.string.bmi)
+                .setPositiveButton(R.string.ok,null)
+                .setNegativeButton(R.string.clear, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         edHeight.setText(" ");
